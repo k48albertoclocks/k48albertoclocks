7d4da5fbd5af60c42380ca3fd1efea42c4763791
k48albertoclocks <110903848+k48albertoclocks@users.noreply.github.com>
2022-08-09 18:13:33 +0700

    upload lần 1

---
+++ data.js
@@ -0,0 +1,30 @@
+var data = [
+  {
+    "id": 1,
+    "name": "Honda",
+    "image_urls": "honda.png",
+    "price": 50,
+    "description": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus rutrum tellus pellentesque eu tincidunt..</p>\n<p>Est ante in nibh mauris cursus mattis molestie a iaculis.</p>"
+  },
+  {
+    "id": 2,
+    "name": "Yamaha",
+    "image_urls": "yamaha.png",
+    "price": 100,
+    "description": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus rutrum tellus pellentesque eu tincidunt..</p>\n<p>Est ante in nibh mauris cursus mattis molestie a iaculis.</p>"
+  },
+  {
+    "id": 3,
+    "name": "SYM",
+    "image_urls": "sym.png",
+    "price": 65,
+    "description": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus rutrum tellus pellentesque eu tincidunt..</p>\n<p>Est ante in nibh mauris cursus mattis molestie a iaculis.</p>"
+  },
+  {
+    "id": 4,
+    "name": "Suzuki",
+    "image_urls": "suzuki.png",
+    "price": 12,
+    "description": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus rutrum tellus pellentesque eu tincidunt..</p>\n<p>Est ante in nibh mauris cursus mattis molestie a iaculis.</p>"
+  }
+];
